ee5c076b4f474744fa8969fc2c6cf063955943aa
SebasVillaLo <villalopezsebastian@gmail.com>
2021-11-16 23:46:52 -0500

    add task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,17 @@
 -- Convert SQL
-CONVERT DATABABE
-	hbtn_0c_0
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE
-	hbtn_0c_0.first_table
-	CONVERT TO CHARACTER SET utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+    hbtn_0c_0.first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE
-	hbtn_0c_0.first_table
-	CONVERT name namet
-	VARCHAR(256)
-	CHARACTER SET utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+    hbtn_0c_0.first_table
+    CHANGE name name
+    VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
