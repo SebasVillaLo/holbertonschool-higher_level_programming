03f0a7d6e935e0ac2ccd3efb70fe87a74737c5b5
SebasVillaLo <villalopezsebastian@gmail.com>
2021-11-17 23:45:22 -0500

    add proyect

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- genre id by show
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show
-LEFT JOIN tv_shows_generes ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_shows_generes
+ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
