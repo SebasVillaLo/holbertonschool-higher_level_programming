6aa11ed98edc76ac584b257820e4077dfce4ca72
SebasVillaLo <villalopezsebastian@gmail.com>
2021-11-17 23:03:39 -0500

    add proyect

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Create new table
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT DEFAUL 1,
+	name VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Show all cities 
+SELECT id, name FROM cities WHERE state_id = (
+	SELECT id FROM state WHERE name = 'California'
+) ORDER BY id DESC;
