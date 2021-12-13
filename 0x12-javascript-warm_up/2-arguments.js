7c8e08c0b67942a4f3cc41757870e1cc07b6ed20
SebasVillaLo <villalopezsebastian@gmail.com>
2021-12-13 16:21:17 -0500

    add task 2, 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
+if (process.argv.slice(2).length > 1) {
 	console.log('Arguments found');
+} else if (process.argv.slice(2).length === 1) {
+	console.log('Argument found');
 } else {
-	console.log('Arguments found');
+	console.log('No argument');
 }
