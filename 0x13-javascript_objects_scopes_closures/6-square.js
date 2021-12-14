69673c7fa75c32687166730fdb5e3e44dfe22f2a
SebasVillaLo <villalopezsebastian@gmail.com>
2021-12-14 14:02:10 -0500

    add proyect

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,10 @@
 const square = require('./5-square');
 module.exports = class Square extends square {
   charPrint (c) {
-    if (c === 'C') {
-      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
-    } else {
+    if (c === undefined) {
       console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    } else {
+      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
     }
   }
 };
