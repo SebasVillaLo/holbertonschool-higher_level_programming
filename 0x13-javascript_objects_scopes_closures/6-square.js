d340ef2ad34470f653932d0e3e8121d4ed9a96ee
SebasVillaLo <villalopezsebastian@gmail.com>
2021-12-14 13:21:20 -0500

    add proyect

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const square = require('./5-square');
 module.exports = class Square extends square {
   charPrint (c) {
     if (c === 'C') {
-      console.log(('C'.repeat(this.width) + '\n').repeat(this.height - 1) + 'C'.repeat(this.width));
+      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
     } else {
       console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
     }
