1ac10d683aaf72ec0be839e3a08975fbb7ca9c4b
SebasVillaLo <villalopezsebastian@gmail.com>
2021-12-15 20:32:41 -0500

    add proyect

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data').dict;
 const newdict = {};
 for (const key in dict) {
   if (newdict[dict[key]] === undefined) {
