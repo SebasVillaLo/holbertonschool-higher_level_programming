7bf67637891a188bee8f8c05a89a1d55d9f1670c
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-08 18:18:33 -0500

    add proyect

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    long = len(list_of_integers)
+
+    if not list_of_integers:
+        return None
+
+    for idx, i in enumerate(list_of_integers):
+
+        if long <= 6:
+            if idx == 0:
+                idx = 1
+            if i > list_of_integers[idx - 1] and i > list_of_integers[idx + 1]:
+                return i
+
+        if i > list_of_integers[idx - 1] and i > list_of_integers[idx + 1]:
+                return i
+
+    return list_of_integers[0]
