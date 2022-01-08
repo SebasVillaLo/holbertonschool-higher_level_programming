59d8fc4d04745b209d18987bd1c53022653c4997
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-08 15:09:23 -0500

    add proyect

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
+"""Request info"""
 
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-    readcontent = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(readcontent)))
-    print("\t- content: {}".format(readcontent))
-    print("\t- utf8 content: {}".format(readcontent.decode('utf8')))
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        readcontent = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(readcontent)))
+        print("\t- content: {}".format(readcontent))
+        print("\t- utf8 content: {}".format(readcontent.decode('utf8')))
