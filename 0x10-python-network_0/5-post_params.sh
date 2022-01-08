d76ec86ea7efc8804f830fb060b7535532f189fb
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-08 17:30:10 -0500

    add proyect

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX "$1" POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
