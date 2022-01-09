50c84cd08e5783624cd743c9d0670cdf59d19c47
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-09 17:55:10 -0500

    add proyect

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv[1]) == 0:
+        q = argv[1]
+    else:
+        q = ""
+
+    try:
+        url = "http://0.0.0.0:5000/search_user"
+        data = {'q': q}
+        r = requests.post(url, data).json()
+
+        if len(r) == 0 or not r.get('id') or not r.get('name'):
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get('id'), r.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
