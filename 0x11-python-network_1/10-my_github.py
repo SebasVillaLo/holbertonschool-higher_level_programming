c1db481e9cf74f3f5579f41db0fc59f8fb66f08d
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-09 18:10:09 -0500

    add proyect

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,3 +6,8 @@ request to http://0.0.0.0:5000/search_user with the letter as a parameter."""
 if __name__ == "__main__":
     import requests
     from sys import argv
+
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(argv[1], argv[2]))
+    res = r.json()
+    print(res.get('id'))
