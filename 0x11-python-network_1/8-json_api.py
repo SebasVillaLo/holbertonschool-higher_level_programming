f90583b6b704953ad8a8fabbeb5f6394fa572836
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-09 17:58:12 -0500

    add proyect

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,19 +7,20 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    if len(argv[1]) > 1:
+    if len(argv) > 1:
         q = argv[1]
     else:
-        q = ""
+        q = ''
 
     try:
-        url = "http://0.0.0.0:5000/search_user"
-        data = {'q': q}
-        r = requests.post(url, data).json()
+        url = 'http://0.0.0.0:5000/search_user'
+        payload = {'q': q}
+        r = requests.post(url, payload).json()
 
         if len(r) == 0 or not r.get('id') or not r.get('name'):
-            print("No result")
+            print('No result')
         else:
-            print("[{}] {}".format(r.get('id'), r.get('name')))
+            print('[{}] {}'.format(r.get('id'), r.get('name')))
+
     except ValueError:
-        print("Not a valid JSON")
+        print('Not a valid JSON')
