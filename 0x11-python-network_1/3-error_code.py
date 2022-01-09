49232a2bdd674f70d99bd18433919538e3f07957
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-09 16:45:05 -0500

    add proyect

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes in a URL,
+sends a request to the URL and displays the body of the response
+(decoded in utf-8)."""
+
+
+if __name__ == "__main__":
+    from urllib import request, error
+    from sys import argv
+
+    try:
+        with request.urlopen(request.Request(argv[1])) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
