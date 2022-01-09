5b17740bc611dda09d35c7278893ee84db0fa579
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-09 17:01:20 -0500

    add proyect

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
 
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r)))
-    print("\t- content: {}".format(r))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
