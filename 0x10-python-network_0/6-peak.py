c3d5cd825406ae5f95d509968cc8f935ccb38f61
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-08 19:06:48 -0500

    add proyect

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(list_of_integers):
+    """search the peak"""
     long = len(list_of_integers)
 
     if not list_of_integers:
@@ -10,7 +11,9 @@ def find_peak(list_of_integers):
 
     return funcion(list_of_integers, 0, long - 1)
 
+
 def funcion(list, start, end):
+    """search the peak"""
     mid = int((start + end) / 2)
 
     if mid + 1 >= len(list) or list[mid + 1] <= list[mid]:
