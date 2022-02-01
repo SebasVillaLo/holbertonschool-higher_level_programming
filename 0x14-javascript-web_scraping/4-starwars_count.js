15ba463b3c58c232f88c406c9191e46e22cd1e19
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-01 18:19:02 -0500

    add proyect

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = process.argv.slice(2);
+const url = 'https://swapi-api.hbtn.io/api/people/18/';
+let count = 0;
+request(argv[0], function (err, r, body) {
+  if (err) { console.log(err); }
+  const string = JSON.parse(body);
+  const rests = string.results;
+  for (const i of rests) {
+    for (const j of i.characters) {
+      if (j === url) {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
