240491f287f242a8c90f8a45d6b0c42fd117dd39
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-01 17:03:43 -0500

    add proyect

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 const request = require('request');
 const argv = process.argv.slice(2);
 
-request('https://swapi-api.hbtn.io/api/films/' + argv[0], function (err, body) {
+request('https://swapi-api.hbtn.io/api/films/' + argv[0], function (err, r, body) {
   if (err) { console.log(err); }
-  const userStr = JSON.parse(body);
-  console.log(userStr.title);
+  const string = JSON.parse(body);
+  console.log(string.title);
 });
