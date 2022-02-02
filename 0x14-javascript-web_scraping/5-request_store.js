3c369512d5724159d7b7e7d41c7e4f9d61cc4c4c
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-01 19:36:13 -0500

    add proyect

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const argv = process.argv.slice(2);
+
+request(argv[0], function (err, r, body) {
+  if (err) { console.log(err); }
+  try {
+    fs.writeFileSync(argv[1], body, 'utf8');
+    // file written successfully
+  } catch (err) {
+    console.error(err);
+  }
+});
