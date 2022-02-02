6b2ca89ec169feb601c3f55e7cda44d8d6958b47
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-01 20:56:37 -0500

    add proyect

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const argv = process.argv.slice(2);
+request(argv[0], function (err, r, body) {
+  if (err) { console.log(err); }
+  const Res = {};
+  const string = JSON.parse(body);
+  for (const i of string) {
+    if (i.completed) {
+      if (Res[i.userId] === undefined) {
+        Res[i.userId] = 1;
+      } else {
+        Res[i.userId] += 1;
+      }
+    }
+  }
+  console.log(Res);
+});
